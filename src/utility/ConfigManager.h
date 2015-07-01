70fae2da4a831643cee14a2e95a443ed88c206e8
Johan Melin <toi@devtoi.eu>
2015-07-01 22:59:10 +0200

    Initial input demo import

---
+++ src/utility/ConfigManager.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Alloc.h"
+#include "CallbackConfig.h"
+
+#define g_ConfigManager ConfigManager::GetInstance()
+
+class ConfigManager
+{
+public:
+	UTILITY_API static ConfigManager& GetInstance();
+
+	UTILITY_API ConfigManager();
+	UTILITY_API ~ConfigManager();
+	UTILITY_API CallbackConfig* GetConfig( const rString& path );
+	UTILITY_API void			SaveDirty( );
+	UTILITY_API const rString&	GetConfigFolderPath( ) const;
+	UTILITY_API void 			SetConfigFolderPath( const rString& path );
+
+private:
+	rMap<rString, CallbackConfig*> 	m_Configs;
+	rString 						m_ConfigFolderPath = "";
+};
